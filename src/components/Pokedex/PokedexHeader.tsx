6ce2b36835891724fe4b4f02efc4dbb712e6ba42
kenzotakamori <marceloktakamori@gmail.com>
2021-09-25 13:21:15 -0300

    creating navbar

--- src/components/Pokedex/PokedexHeader.tsx
+++ src/components/Pokedex/PokedexHeader.tsx
@@ -7,18 +7,27 @@ const PokedexHeader = (props: any) => {
   return (
     <header>
       <div className={props.isOpen ? 'red-section header-open' : 'red-section'}>
-        <CSSTransition
-          classNames="poke-logo"
-          in={props.isOpen}
-          timeout={{
-            appear: 1000,
-            enter:1000,
-            exit: 1000,
-          }}
-          unmountOnExit
-        >
-          <img alt="logo" src={pokeLogo}/>
-        </CSSTransition>
+        <nav>
+          <ul>
+            <li key="home">Opção 1</li>
+            <li key="home-2">Opção 2</li>
+            <CSSTransition
+              classNames="poke-logo"
+              in={props.isOpen}
+              timeout={{
+                appear: 1000,
+                enter:1000,
+                exit: 1000,
+              }}
+              unmountOnExit
+            >
+              <img alt="logo" src={pokeLogo}/>
+            </CSSTransition>
+            <li key="home-3">Opção 3</li>
+            <li key="home-4">Opção 4</li>
+          </ul>
+        </nav>
+        
       </div>
       <div className="black-section">
         <div className="black-circle">
